6f04312e141ca379971e3a5a2db3fed72352d7d3
Александр Орехов <sfz2120@mail.ru>
2025-03-28 02:09:06 +0500

    Правка селеноид урла

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -52,7 +52,7 @@ def driver(request):
         })
 
         driver = webdriver.Remote(
-            command_executor="http://selenoid:4444/wd/hub",
+            command_executor="http://localhost:4444/wd/hub",
             options=options,
         )
     else:
