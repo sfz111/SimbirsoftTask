4f5282f1978e52fef49625a354eb48f8b2b93ece
Александр Орехов <sfz2120@mail.ru>
2025-03-29 02:56:26 +0500

    Добавил сообщения к ассертам, поправил проверку создания клиента

--- tests/ui/test_customers.py
+++ tests/ui/test_customers.py
@@ -14,8 +14,8 @@ class TestCustomers:
             add_customer_page.add_customer()
 
         with step("Проверяем, что появилось сообщение об успешном создании клиента"):
-            text = add_customer_page.switch_to_alert_and_get_text()
-            assert "Customer added successfully with customer id" in text, f"Ожидалось сообщение об успешном создании клиента, но получено: '{text}'"
+            text = add_customer_page.switch_to_alert_and_get_text().split(" :")[0]
+            assert "Customer added successfully with customer id" == text, f"Ожидалось сообщение об успешном создании клиента, но получено: '{text}'"
 
     @allure.title("Сортировка клиентов по имени")
     def test_sort_customers(self, manager_page, customers_page):
@@ -25,12 +25,12 @@ class TestCustomers:
         with step(f"Сортируем клиентов по столбцу '{FIRST_NAME}'"):
             actual_list = customers_page.sort_customers_by_first_name()
         with step("Проверяем что клиенты отсортированы по убыванию"):
-            assert sorted(actual_list, reverse=True) == actual_list
+            assert sorted(actual_list, reverse=True) == actual_list, "Клиенты не отсортированы по убыванию"
 
         with step(f"Еще раз сортируем клиентов по столбцу '{FIRST_NAME}'"):
             actual_list = customers_page.sort_customers_by_first_name()
         with step("Проверяем что клиенты отсортированы по возрастанию"):
-            assert sorted(actual_list) == actual_list
+            assert sorted(actual_list) == actual_list, "Клиенты не отсортированы по возрастанию"
 
     @allure.title("Удаление клиента")
     def test_delete_customer(self, manager_page, customers_page):
